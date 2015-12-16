83456f7bed2634e50366c9a2e8a4add0674115c1
kcrowe01 <Kenneth.Crowell@tufts.edu>
2015-12-15 19:49:30 -0500

    fixing camera for mobile

---
+++ PuzzDuel/Assets/cam.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+function Start () {
+	var camera : Camera = GameObject.FindWithTag("MainCamera").GetComponent(Camera);
+	camera.aspect = 750.0/1334.0;
+}
+
+function Update () {
+
+}
